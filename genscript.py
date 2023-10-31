00ddde313cd16b92966d54e918044f7697b6c767
Alex Carter <alexcarter@Alexs-MacBook-Pro.local>
2023-10-31 19:34:21 -0400

    cleanup and generation improvements

--- genscript.py
+++ genscript.py
@@ -5,18 +5,22 @@ import os
 import openai
 import re
 
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-
-openai.api_key = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Custom exception
 class CustomError(Exception):
     pass
 
+main_char = "Alice"
+char_list = ["Alice", "Bob", "Carly"]
+
+user_content_template = "I want you to write a story with {} speaking characters: {}. Write this script to be as lengthy as you can.  {} is \"the main character\", all dialogue should include {} as either CharacterNameSpeaking or CharacterNameReceiving. This story should be communicated entirely over text messages. do not include parenthetical comments, stage directions, or information about the characters, just their text-message based dialogue. Please use common text abbreviations, lots of emojis, etc. It should involve relationships, gossip, and be very trashy. the more unexpected twists the better. The story should be formatted as\n(CharacterNameSpeaking, CharacterNameReceiving) Dialogue\n Lengthy pieces of dialogue can be broken to multiple lines, each prefaced with the same (CharacterNameSpeaking, CharacterNameReceiving) tag"
+user_content = user_content_template.format(len(char_list), ', '.join(char_list), main_char, main_char)
+
 # Define the conversation history
 conversation = [
-    {"role": "system", "content": "You are a contemporary drama writer, skilled in creating dialogue that is captivating and full of surprises. Stylistically, you begin every line of dialogue with a (CharacterNameSpeaking, CharacterNameReceiving) tag. Stylistically, you do not add extra linebreaks between different characters speaking. Stylistcally, you often breakdown one piece of dialogue into two shorter messages, with the same (CharacterNameSpeaking, CharacterNameReceiving) tag"},
-    {"role": "user", "content": "I want you to write a story with 3 speaking characters: Alice, Bob, and Carly as long as you can.  I want you to pick one of the character who is \"the main character\", all dialogue should include them as either speaker or receiver. This story should be communicated entirely over text messages. do not include parenthetical comments, stage directions, or information about the characters, just their text-message based dialogue. Do not add empty lines between dialogue. Please use common text abbreviations, lots of emojis, etc. It should involve relationships, gossip, and be very trashy. the more unexpected twists the better. The story should be formatted as\n(CharacterNameSpeaking, CharacterNameReceiving) Dialogue\n Lengthy pieces of dialogue can be broken to multiple lines, each prefaced with the same (CharacterNameSpeaking, CharacterNameReceiving) tag"}
+    {"role": "system", "content": "You are a contemporary drama writer, skilled in creating dialogue that is captivating and full of surprises. Stylistically, you begin every line of dialogue with a (CharacterNameSpeaking, CharacterNameReceiving) tag. Stylistcally, you often breakdown one piece of dialogue into two shorter messages, with the same (CharacterNameSpeaking, CharacterNameReceiving) tag"},
+    {"role": "user", "content": user_content}
 ]
 
 # Initial script API call
@@ -29,39 +33,20 @@ script = completion.choices[0]['message']['content']
 print(script)
 
 # Validate script format
-pattern = r'\(([^,]+),\s*([^)]+)\)\s*(.+)'
+script_lines = script.splitlines()
 
-if len(script.split('\n')) < 20:
-    print("script too short")
+if len(script_lines) < 20:
+    print("ERROR script too short")
     raise CustomError("script too short")
 
-mc_question = "I am looking for a one word answer: who is the main character? do not include any extra words"
-conversation.append({"role": "user", "content": mc_question})
-
-# Make another API call with the extended conversation
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=conversation
-)
-
-main_char = completion.choices[0]['message']['content']
-print(main_char)
-
-chars_question = "please give a set of the characters in the format [character1, character2, character3]. do not include any unncessary words"
-conversation.append({"role": "user", "content": chars_question})
-
-# Make another API call with the extended conversation
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=conversation
-)
-
-# Extract and print the response to the follow-up question
-char_list = completion.choices[0]['message']['content']
-print(char_list)
+# regex for character tag
+pattern = r'\(([^,]+),\s*([^)]+)\)\s*(.+)'
 
 # Validate Main character use and existance of speaking characters
-for line in script.split('\n'):
+for line in script_lines:
+    if len(line) < 3:
+        continue
+    print("line",  line)
     match = re.match(pattern, line)
     if match:
         character1 = match.group(1)
@@ -69,19 +54,19 @@ for line in script.split('\n'):
         dialogue = match.group(3)
         print(f"Character1: {character1}, Character2: {character2}, Dialogue: {dialogue}")
         if not (character1 == main_char or character2 == main_char):
-            print("main character not involved in dialogue")
-            raise CustomError("main character not involved in dialogue")
-            break
+            print("ERROR main character not involved in dialogue")
+            #raise CustomError("main character not involved in dialogue")
+            #break
         if not (character1 in char_list):
-            print("character1 does not exist")
-            raise CustomError("character1 does not exist")
-            break
+            print("ERROR character1 does not exist")
+            #raise CustomError("character1 does not exist")
+            #break
         if not (character2 in char_list):
-            print("character2 does not exist")
-            raise CustomError("character2 does not exist")
-            break
+            print("ERROR character2 does not exist")
+            #raise CustomError("character2 does not exist")
+            #break
     else:
-        print("line does not contain valid tag")
+        print("ERROR line does not contain valid tag")
         raise CustomError("line does not contain valid tag")
         break
 
@@ -102,9 +87,9 @@ os.environ[env_var_name] = str(counter)
 # Create the file name with an incremented counter
 file_name = f"script{counter}.txt"
 
-with open(file_path, 'w') as file:
+with open(file_name, 'w') as file:
     file.write(main_char + "\n")
-    file.write(char_list + "\n")
+    file.write(str(char_list) + "\n")
     file.write(script)
 
 print(f"Script and data has been written to {file_name}")
